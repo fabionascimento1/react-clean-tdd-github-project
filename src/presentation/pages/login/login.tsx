2959e83767326c3ed6a3f9e179979d69f9074da3
Fabio Nascimento <agsuperpoder@gmail.com>
2022-02-13 21:48:51 -0300

    feat: config webpack and add page login test

---
+++ src/presentation/pages/login/login.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Styles from './login-styles.scss'
+
+const Login: React.FC = () => {
+  return (
+    <div className={Styles.login}>
+      Hello 1
+    </div>
+  )
+}
+
+export default Login
