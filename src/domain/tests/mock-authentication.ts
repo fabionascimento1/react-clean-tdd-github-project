bc370d4bb0ae179f9a79eca6dafdf89654634aa4
Fabio Nascimento <agsuperpoder@gmail.com>
2022-02-18 11:15:21 -0300

    feat: ensure remoteAuthentication return an account on sucess

--- src/domain/tests/mock-authentication.ts
+++
@@ -1,6 +0,0 @@
-import { AuthenticationParams } from '../usecases/authentication'
-
-export const mockAuthentication = (): AuthenticationParams => ({
-  email: 'agsuperpoder@gmail.com',
-  password: '12345'
-})
