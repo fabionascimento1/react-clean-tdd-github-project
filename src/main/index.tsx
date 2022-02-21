aad9a30a4625083920d9c76d7c9f88e409250c90
Fabio Nascimento <agsuperpoder@gmail.com>
2022-02-21 08:08:27 -0300

    feat: add Index Page

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,7 +1,8 @@
+import Index from '@/presentation/pages'
 import React from 'react'
 import ReactDOM from 'react-dom'
 
 ReactDOM.render(
-  <></>,
+  <Index />,
   document.getElementById('main')
 )
---
+++ src/presentation/pages/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './index.scss'
+
+const Index: React.FC = () => {
+  return (
+    <>Index</>
+  )
+}
+
+export default Index
