1ff480cf5016160059a1b32bfd87091946dd894c
Fabio Nascimento <agsuperpoder@gmail.com>
2022-02-22 11:35:39 -0300

    feat: add search form component

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,6 +1,7 @@
 import Index from '@/presentation/pages'
 import React from 'react'
 import ReactDOM from 'react-dom'
+import '@/presentation/styles/global.scss'
 
 ReactDOM.render(
   <Index />,
--- src/presentation/pages/index.tsx
+++ src/presentation/pages/index.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
-import './index.scss'
+import Logo from '../components/logo'
+import Spinner from '../components/spinner'
+import Styles from './index-styles.scss'
 
 const Index: React.FC = () => {
   return (
-    <>Index</>
+    <>
+      <header className={Styles.header}>
+        <Logo />
+        <form className={Styles.form}>
+          <input type='text' name='repository' placeholder='Procurar por repositório ...' />
+          <button type='submit'>Pesquisar</button>
+        </form>
+        <div className={Styles.errorWrap}>
+          <Spinner className={Styles.Spinner} />
+          <span className={Styles.error}>Error</span>
+        </div>
+      </header>
+    </>
   )
 }
 
