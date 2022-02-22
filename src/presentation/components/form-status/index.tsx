91542753f4849f0b73ae6685400fc033b37dad2e
Fabio Nascimento <agsuperpoder@gmail.com>
2022-02-22 16:00:50 -0300

    feat: not render spinner and error on start

--- src/presentation/components/form-status/index.tsx
+++ src/presentation/components/form-status/index.tsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Spinner from '../spinner'
 import Styles from './form-status-styles.scss'
+import Context from '@/presentation/contexts/form-contexts'
 
 const FormStatus: React.FC = () => {
+  const { isLoading, errorMessage } = useContext(Context)
   return (
-    <div className={Styles.errorWrap}>
-      <Spinner className={Styles.Spinner} />
-      <span className={Styles.error}>Error</span>
+    <div data-testid="error-wrap" className={Styles.errorWrap}>
+      { isLoading && <Spinner className={Styles.Spinner} /> }
+      { errorMessage && <span className={Styles.error}>Error</span> }
     </div>
   )
 }
--- src/presentation/pages/index.tsx
+++ src/presentation/pages/index.tsx
@@ -1,19 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import FormStatus from '../components/form-status'
 import Input from '../components/input'
 import Logo from '../components/logo'
 import Styles from './index-styles.scss'
+import Context from '@/presentation/contexts/form-contexts'
+
+type StateProps = {
+  isLoading: boolean
+  errorMessage: string
+}
 
 const Index: React.FC = () => {
+  const [state] = useState<StateProps>({
+    isLoading: false,
+    errorMessage: ''
+  })
+
   return (
     <>
       <header className={Styles.header}>
         <Logo />
-        <form className={Styles.form}>
-         <Input type='text' name='repository' placeholder='Procurar por repositório ...' />
-          <button type='submit'>Pesquisar</button>
-        </form>
-        <FormStatus />
+        <Context.Provider value={state}>
+          <form className={Styles.form}>
+          <Input type='text' name='repository' placeholder='Procurar por repositório ...' />
+            <button type='submit'>Pesquisar</button>
+          </form>
+          <FormStatus />
+        </Context.Provider>
       </header>
     </>
   )
