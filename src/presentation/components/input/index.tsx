c1ba5cf1b04e2fba8641dc8c4ca8c08909dcd3fa
Fabio Nascimento <agsuperpoder@gmail.com>
2022-02-23 09:35:17 -0300

    feat: ensure login starts with inputs with correct state

--- src/presentation/components/input/index.tsx
+++ src/presentation/components/input/index.tsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import Context from '@/presentation/contexts/form-contexts'
+import React, { useContext } from 'react'
 import Styles from './input-styles.scss'
 
 type Props = React.InputHTMLAttributes<HTMLInputElement>
 
 const Input: React.FC<Props> = (props: Props) => {
+  const value = useContext(Context)
+  const error = value['searchError']
+
+  const getStatus = (): string => {
+    return '🔴'
+  }
+  const getTitle = (): string => {
+    return error
+  }
   return (
     <div className={Styles.inputWrap}>
        <input {...props} />
-       <span className={Styles.status}>🔴</span>
+       <span title={getTitle()} data-testid='search-status' className={Styles.status}>{getStatus()}</span>
     </div>
   )
 }
--- src/presentation/pages/index.tsx
+++ src/presentation/pages/index.tsx
@@ -5,15 +5,11 @@ import Logo from '../components/logo'
 import Styles from './index-styles.scss'
 import Context from '@/presentation/contexts/form-contexts'
 
-type StateProps = {
-  isLoading: boolean
-  errorMessage: string
-}
-
 const Index: React.FC = () => {
-  const [state] = useState<StateProps>({
+  const [state] = useState({
     isLoading: false,
-    errorMessage: ''
+    errorMessage: '',
+    searchError: 'Campo obrigatório'
   })
   return (
     <>
@@ -21,7 +17,7 @@ const Index: React.FC = () => {
         <Logo />
         <Context.Provider value={state}>
           <form className={Styles.form}>
-          <Input type='text' name='repository' placeholder='Procurar por repositório ...' />
+          <Input type='text' name='search' placeholder='Procurar por repositório ...' />
             <button data-testid='submit' disabled type='submit'>Pesquisar</button>
           </form>
           <FormStatus />
