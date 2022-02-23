66c2da6909aef1ef78fcb40fe55e2d423e5d0100
Fabio Nascimento <agsuperpoder@gmail.com>
2022-02-23 11:19:54 -0300

    test: ensure search call validation with corret value

--- src/presentation/pages/index.spec.tsx
+++ src/presentation/pages/index.spec.tsx
@@ -1,19 +1,35 @@
 import React from 'react'
-import { render, RenderResult } from '@testing-library/react'
+import { cleanup, fireEvent, render, RenderResult } from '@testing-library/react'
 import Index from '.'
+import { Validation } from '@/presentation/protocols/validation'
 
 type SutTypes = {
   sut: RenderResult
+  validationSpy: ValidationSpy
+}
+
+class ValidationSpy implements Validation {
+  errorMessage: string
+  input: object
+
+  validate (input: object): string {
+    this.input = input
+    return this.errorMessage
+  }
 }
 
 const makeSut = (): SutTypes => {
-  const sut = render(<Index />)
+  const validationSpy = new ValidationSpy()
+  const sut = render(<Index validation={validationSpy} />)
   return {
-    sut
+    sut,
+    validationSpy
   }
 }
 
 describe('', () => {
+  afterEach(cleanup)
+
   test('Should start render with initial state', () => {
     const { sut } = makeSut()
     const errorWrap = sut.getByTestId('error-wrap')
@@ -23,5 +39,14 @@ describe('', () => {
     const searchStatus = sut.getByTestId('search-status')
     expect(searchStatus.title).toBe('Campo obrigatório')
     expect(searchStatus.textContent).toBe('🔴')
+  })
+
+  test('Should call validation with correct value', () => {
+    const { sut, validationSpy } = makeSut()
+    const searchInput = sut.getByTestId('search')
+    fireEvent.input(searchInput, { target: { value: 'any' } })
+    expect(validationSpy.input).toEqual({
+      search: 'any'
+    })
   })
 })
